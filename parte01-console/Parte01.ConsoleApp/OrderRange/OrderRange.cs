62ada49766af2fcc364c28dff7db7fded69000af
JeanPaulLazaroQuispe <126197721+JeanPaulLazaroQuispe@users.noreply.github.com>
2026-02-14 17:40:11 -0500

    implementar OrderRange y OrderResult
    Caso 1 completado: retorna listas de números pares e impares en orden ascendente.

---
+++ parte01-console/Parte01.ConsoleApp/OrderRange/OrderRange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte01.ConsoleApp.OrderRange
+{
+    public class OrderRange
+    {
+        public OrderResult Build(IEnumerable<int> numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException(nameof(numbers));
+
+            var evens = numbers
+                .Where(n => n > 0 && n % 2 == 0)
+                .Order()
+                .ToList();
+
+            var odds = numbers
+                .Where(n => n > 0 && n % 2 != 0)
+                .Order()
+                .ToList();
+
+            return new OrderResult
+            {
+                Evens = evens,
+                Odds = odds
+            };
+        }
+    }
+}
